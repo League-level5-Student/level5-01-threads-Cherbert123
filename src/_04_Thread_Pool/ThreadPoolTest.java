24f7a55c47ce71c83fa0fbcdfdbf2caeb5e4dbcb
Charlie <league@WTS14-3.attlocal.net>
2019-01-27 12:58:31 -0800

    3

--- src/_04_Thread_Pool/ThreadPoolTest.java
+++ src/_04_Thread_Pool/ThreadPoolTest.java
@@ -1,18 +1,10 @@
 package _04_Thread_Pool;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
 
 import java.util.Random;
 
 import org.junit.Test;
-
-import thread_pool.ThreadPool;
-//   A Thread Pool is a way of managing multiple threads with multiple workloads.
-//   It works by collecting a list of tasks to be performed by a thread.
-//   A pool of threads is then created, and each thread takes a task from the list and does that work.
-//   This continues until all the tasks are done.
-//   When all the tasks are complete, the thread pool has finished its job.
-//   This test will use a Thread pool to convert a large amount of lower case characters to upper case
 
 //1. Fill in the ThreadPool and Worker classes and the Task interface according to the Thread Pool UML Diagram.
 
@@ -20,35 +12,35 @@ public class ThreadPoolTest {
 
 	@Test
 	public void test() {
-		int total = 536870912;
-		
+		int total = 100000;
+		int cores = Runtime.getRuntime().availableProcessors();
 		ThreadPool tp = new ThreadPool(4);
 		char[] chars = new char[total];
-		
-		for(int i = 0; i < total; i++){
-			chars[i] = (char)(new Random().nextInt(25) + 97);
+
+		for (int i = 0; i < total; i++) {
+			chars[i] = (char) (new Random().nextInt(25) + 97);
 		}
-				
+
 		int count = (int) Math.sqrt(total);
-		
-		for(int i = 0; i < count; i++){
+
+		for (int i = 0; i < count; i++) {
 			int x = i * count;
-			tp.addTask(()->{
-				for(int j = 0; j < count; j++){
+			tp.addTask(() -> {
+				for (int j = 0; j < count; j++) {
 					chars[x + j] = Character.toUpperCase(chars[x + j]);
 				}
 			});
 		}
 
 		long start = System.currentTimeMillis();
-		
+
 		tp.start();
-		
+
 		long end = System.currentTimeMillis() - start;
 		System.out.println("Total time: " + end);
-		
-		for(char c : chars){
+
+		for (char c : chars) {
 			assertTrue(Character.isUpperCase(c));
-		}	
+		}
 	}
 }
